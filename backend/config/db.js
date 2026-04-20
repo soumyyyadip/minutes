2852900099692d602a2415f0c847ebcbf5df4f63
Soum <soumy18a@gmail.com>
2026-04-20 13:51:01 +0530

    fixed uri leak

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://som:123@cluster0.bex6qwc.mongodb.net/meetflow-b?appName=Cluster0');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error connecting to MongoDB: ${err.message}`);
